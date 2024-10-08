fc4b142f10980ab9570c0b2da55fcd53b6bfa6bb
GustavoLyra23 <gustavolyra23@gmail.com>
2024-10-08 16:25:54 -0300

    first commit

---
+++ src/main/java/com/gustavolyra/twiter_copy/TwiterCopyApplication.java
@@ -0,0 +1,13 @@
+package com.gustavolyra.twiter_copy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwiterCopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwiterCopyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavolyra/twiter_copy/TwiterCopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavolyra.twiter_copy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwiterCopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
